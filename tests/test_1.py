41b600313c1a8265dbaa639651ed0124610d1bbb
marekkokot <marek.koki@gmail.com>
2026-08-21 13:03:02 +0200

    Update EXPECTED_MD5 values for version changes

--- tests/test_1.py
+++ tests/test_1.py
@@ -10,8 +10,9 @@ from pathlib import Path
 #EXPECTED_MD5 = "473782745f9dc076a6eff1f8a2f0c834" # for 2.0.0
 #EXPECTED_MD5 = "a256506cbf1443e2ec536b75ba897dc9" # for 2.0.1
 #EXPECTED_MD5 = "21b3e97c8aa87c7e156a1883cef83699" # for 2.0.2
-EXPECTED_MD5 = "6538947bed991d35db21b79ad99d3d1e"  # for 2.0.3
-EXPECTED_MD5 = "1a5d2494511e6c2fdcfafc6e00e3098c"  # for 2.0.4
+#EXPECTED_MD5 = "6538947bed991d35db21b79ad99d3d1e"  # for 2.0.3
+#EXPECTED_MD5 = "1a5d2494511e6c2fdcfafc6e00e3098c"  # for 2.0.4
+EXPECTED_MD5 = "4bb7d9458a1edc39e154dc1684d7d21d"  # for 2.0.5
 
 # Directory where this script lives
 HERE = Path(__file__).resolve().parent
